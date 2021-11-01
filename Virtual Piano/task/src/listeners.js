ac33c852598215c04ed96f072d5be4efe30dba37
Mykhailo Hariachyi <m.gariachiy@gmail.com>
2021-11-01 17:19:28 -0400

    Finished stage 5 Added mouse clicks Added animation

--- Virtual Piano/task/src/listeners.js
+++ Virtual Piano/task/src/listeners.js
@@ -1,34 +1,50 @@
+const whiteKeys = ['A', 'S', 'D', 'F', 'G', 'H', 'J'];
+const blackKeys = ['W', 'E', 'T', 'Y', 'U'];
+
 const keyMsg = (key) => `The '${key}' key is pressed.`;
-const play = (key) => {
+const keyErrorMsg = (key) => `The key ${key} is not supported!`;
+
+const playSound = (keyGroup, key) => {
     console.log(keyMsg(key));
-    const audio = new Audio(`./resources/white_keys/${key}.mp3`)
-    audio.play().then(r => r);
-} 
+    return new Audio(`./resources/${keyGroup}/${key}.mp3`).play();
+};
+const playWhite = (key) => playSound('white_keys', key);
+const playBlack = (key) => playSound('black_keys', key);
+
+const isWhite = (key) => whiteKeys.includes(key.toUpperCase());
+const isBlack = (key) => blackKeys.includes(key.toUpperCase());
 
 document.addEventListener('keydown', (ev) => {
-    switch (ev.code) {
-        case "KeyA":
-            play('A')
-            break;
-        case "KeyS":
-            play('S');
-            break;
-        case "KeyD":
-            play('D');
-            break;
-        case "KeyF":
-            play('F');
-            break;
-        case "KeyG":
-            play('G');
-            break;
-        case "KeyH":
-            play('H');
-            break;
-        case "KeyJ":
-            play('J');
-            break;
-        default:
-            console.log(`The key ${ev.code} is not supported!`);
+    if (ev.repeat) {
+    } else if (ev.code.startsWith('Key')) {
+        const key = ev.code[3];
+        if (isWhite(key)) {
+            playWhite(key);
+            document.getElementById(key).style.backgroundImage = 'linear-gradient(to bottom, white, lightgray)';
+        } else if (isBlack(key)) {
+            playBlack(key);
+            document.getElementById(key).style.backgroundImage = 'linear-gradient(to bottom, black, #505050)';
+        } else {
+            console.log(keyErrorMsg(key));
+        }
+    } else {
+        console.log(keyErrorMsg(ev.code));
     }
 })
+
+document.addEventListener('keyup', (ev) => {
+    const key = ev.code
+    if (key.startsWith('Key') && (isWhite(key[3]) || isBlack(key[3]))) {
+        document.getElementById(key[3]).style.backgroundImage = '';
+    }
+})
+
+const whiteKeysButtons = document.getElementsByClassName('white-keys')[0].children;
+for (let key of whiteKeysButtons) {
+    key.addEventListener('click', () => playWhite(key.id));
+}
+
+const blackKeysButtons = document.getElementsByClassName('black-keys')[0].children;
+for (let key of blackKeysButtons) {
+    key.addEventListener('click', () => playBlack(key.id));
+}
